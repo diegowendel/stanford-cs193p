af1d826efbf07bb2b7390fc072e0f24b8de8d05f
diegoferreira <diegowendelf@gmail.com>
2020-07-11 00:33:56 -0300

    Set up Memorize app

---
+++ Memorize/MemoryGame.swift
@@ -0,0 +1,34 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by Diego Ferreira on 08/07/20.
+//  Copyright © 2020 Diego Ferreira. All rights reserved.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    var cards: Array<Card>
+    
+    func choose(card: Card) {
+        print("card chosen: \(card)")
+    }
+    
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = Array<Card>()
+        
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(id: pairIndex*2, content: content))
+            cards.append(Card(id: pairIndex*2+1, content: content))
+        }
+    }
+    
+    struct Card: Identifiable {
+        var id: Int
+        var isFaceUp: Bool = true
+        var isMatched: Bool = false
+        var content: CardContent
+    }
+}
